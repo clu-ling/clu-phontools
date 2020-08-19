e040240f15f4d26f4b9d821f800d2813fba334d5
Elsayedissa <elsayed.sabry.issa@gmail.com>
2020-08-18 23:30:03 -0700

    add complete code for realine OOP

--- realine/main.py
+++ realine/main.py
@@ -1,35 +1,55 @@
+import aline
 from aline import ReAline
+#from aline import ReAline.align as al
 import pandas as pd
 import numpy as np
 
+al = ReAline()
+
+# reading the file
 def read_files(path):
     file = pd.read_excel(path, header=None)
-    
-    return file
+    df = file.stack().groupby(level=0).apply(''.join)
+    # list of lists
+    K = 'LB'
+    list_of_lists = []
+    for i in df:
+        res = [i for j in i.split(K) for i in (j, K)][:-1]
+        res = [i for i in res if i]
+        list_of_lists.append(res)
+    # list of tuples
+    v = iter(list_of_lists)
+    list_of_tuples = [(i, next(v)) for i in v]
+    another_list = []
+    for i in list_of_tuples:
+        s = list(zip(i[0], i[1]))
+        another_list.append(s)
+
+    return another_list
+
+def do_align(a):
+    alignments = []
+    for L in a:
+        print ('--------------------')
+        print (L)
+        print ('--------------------')
+        for pair in L:
+            alignment = al.align(pair[0], pair[1])[0]
+            alignment = ['({})'.format(a) for a in alignment]
+            alignment = ' '.join(alignment)
+            alignment = ('{} ~ {} : {}'.format(pair[0], pair[1], alignment))
+            alignments.append(alignment)
+            print (alignment)
+    return alignments
 
 
-s = read_files('alignments.xlsx')
-print (s)
+# save to file
+read_file = read_files('./data/alignments.xlsx')  # list of lists of tuples
+#print (s)
+alignments = do_align(read_file)
 
 
-
-
-
-# cat = ReAline()
-# print(cat)
-# a = cat.sigma_skip('p')
-# print(a)
-# b = cat.V('p')
-# print(b)
-# c = cat.R('p', 'b')
-# print(c)
-# d = cat.diff('p', 'b', 'place')
-# print(d)
-# e = cat.delta('p', 'b')
-# print(e)
-# f = cat.sigma_sub('p', 'b')
-# print(f)
-# g = cat.sigma_exp('p', 'bi')
-# print(g)
-# h = cat.align('cat', 'bat')
-# print(h)
+df = pd.DataFrame()
+# Creating a column
+df['alignments'] = alignments
+df.to_excel('./results/results.xlsx', index= False)
