8307e0c0d36847f67549daaa08f78d4ee62b5270
Elsayedissa <elsayed.sabry.issa@gmail.com>
2021-08-17 17:39:11 -0700

    add Actions class and some tests

--- clu/phontools/alignment/parser.py
+++ clu/phontools/alignment/parser.py
@@ -1,10 +1,58 @@
+# -*- coding: utf-8 -*-
+
+from dataclasses import dataclass
+from enum import Enum, auto
+from clu.phontools.alignment.realine import ReAline
+from typing import Dict, Text, Tuple, List, Optional, Sequence, Callable
+
+
+class Actions(Enum):
+    """
+    The actions used by the parser.
+    """
+    DISCARD_T = auto()
+    DISCARD_G = auto()
+    SHIFT_T = auto()
+    SHIFT_G = auto()
+    STACK_SWAP = auto()
+    INSERTION_PRESERVE_COPY_CHILD = auto()
+    INSERTION_PRESERVE_COPY_PARENT = auto()
+    DELETION_PRESERVE_COPY_CHILD = auto()
+    DELETION_PRESERVE_COPY_PARENT = auto()
+    ALIGN = auto()
+    SUBSTITUTION = auto()
+
+    def describe(self):
+        return self.name, self.value
+
+
+@dataclass()
+class Graph:
+    """
+    This dataclass encodes all the aligment output (edges, vertics, and nodes)
+    wether these are transcript or gold. Based on this graph, we constrcut the oracle
+    """
+    pass
 
 
 
 class Parser:
-    pass
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def add_special_symbol(text: Text) -> Text:
+        symbol = text[0]
+        for item in text[1:]:
+            symbol += '-' + item
+        return '-' + symbol + '-'
+
 
 class Oracle:
-    @staticmethod
-    def greet():
-        return 'Hello'
+    pass
+
+
+
+
+
+
--- tests/test_oracle.py
+++ tests/test_oracle.py
@@ -2,7 +2,59 @@
 
 import unittest
 from clu.phontools.alignment.realine import ReAline
-from clu.phontools.alignment.parser import Oracle
+from clu.phontools.alignment.parser import Oracle, Parser, Actions
+
+"""
+Test `clu.phontools.alignment.parser.Actions` behaviors
+"""
+
+class ActionsTests(unittest.TestCase):
+    def test_actions(self):
+        """`clu.phontools.alignment.parser.Actions.describe()` should return the correct enumerations of the actions used by the parser
+        """
+        assert len(Actions) == 11
+
+        assert Actions.DISCARD_T.describe() == ('DISCARD_T', 1)
+        assert Actions.DISCARD_G.describe() == ('DISCARD_G', 2)
+        assert Actions.SHIFT_T.describe() == ('SHIFT_T', 3)
+        assert Actions.SHIFT_G.describe() == ('SHIFT_G', 4)
+        assert Actions.STACK_SWAP.describe() == ('STACK_SWAP', 5)
+        assert Actions.INSERTION_PRESERVE_COPY_CHILD.describe() == (
+            'INSERTION_PRESERVE_COPY_CHILD', 6)
+        assert Actions.INSERTION_PRESERVE_COPY_PARENT.describe() == (
+            'INSERTION_PRESERVE_COPY_PARENT', 7)
+        assert Actions.DELETION_PRESERVE_COPY_CHILD.describe() == (
+            'DELETION_PRESERVE_COPY_CHILD', 8)
+        assert Actions.DELETION_PRESERVE_COPY_PARENT.describe() == (
+            'DELETION_PRESERVE_COPY_PARENT', 9)
+        assert Actions.ALIGN.describe() == ('ALIGN', 10)
+        assert Actions.SUBSTITUTION.describe() == ('SUBSTITUTION', 11)
+
+        
+
+"""
+Test `clu.phontools.alignment.parser.Parser` behaviors
+"""
+
+class ParserTests(unittest.TestCase):
+    def test_add_special_symbol(self):
+        """`clu.phontools.alignment.parser.Parser.add_special_symbol()` should return 'a string with a special character (-) between its letter/phones'
+        :INPUT: cat
+        :OUTPUT: -c-a-t-
+        """
+        text = 'cat'
+        self.assertEqual(
+            Parser.add_special_symbol(text),
+            '-c-a-t-',
+            f"Parser.add_special_symbol() should return '-c-a-t-'"
+        )
+
+
+
+
+
+
+
 
 
 """
@@ -10,16 +62,15 @@ Test behavior of Oracle
 """
 
 
-class OracleTests(unittest.TestCase):
-    def test_hello(self):
-        """`clu.phontools.alignment.parser.Oracle.greet()` should return 'hello'"""
+# class OracleTests(unittest.TestCase):
+#     def test_hello(self):
+#         """`clu.phontools.alignment.parser.Oracle.greet()` should return 'hello'"""
 
+#         self.assertEqual(
+#             Oracle.greet(),
+#             'Hello',
+#             f"Oracle.greet() should return 'hello', but {Oracle.greet()} was returned",
+#         )
 
-
-        self.assertEqual(
-            Oracle.greet(),
-            'Hello',
-            f"Oracle.greet() should return 'hello', but {Oracle.greet()} was returned",
-        )
 
  
