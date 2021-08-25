50484aa0d0625958ec976d7393958d8f5f5af405
Elsayedissa <elsayed.sabry.issa@gmail.com>
2021-08-24 22:00:18 -0700

    adding some changes to (parser.py) and (test_oracle.py)

--- clu/phontools/alignment/parser.py
+++ clu/phontools/alignment/parser.py
@@ -6,10 +6,37 @@ from clu.phontools.alignment.realine import ReAline
 from typing import Dict, Text, Tuple, List, Optional, Sequence, Callable
 
 
+class Symbol:
+    # FIXME: need to track index of symbol
+    def __init__(self, char: Text):
+        self._symbol = char
+
+    def __repr__(self):
+        return self._symbol
+
+
+class IntermediateSymbol(Symbol):
+    def __init__(self):
+        super().__init__(char="NULL")
+
+
+def prepare_symbols(word: Text) -> Sequence[Symbol]:
+    res = [IntermediateSymbol()]
+    for char in word:
+        res.append(Symbol(char))
+        res.append(IntermediateSymbol())
+    return res
+
+
+def assign_index(symbols: List) -> List[Tuple[int, Symbol]]:
+    return [(indx, symbol) for indx, symbol in enumerate(symbols)]
+
+
 class Actions(Enum):
     """
     The actions used by the parser.
     """
+
     DISCARD_T = auto()
     DISCARD_G = auto()
     SHIFT_T = auto()
@@ -32,8 +59,8 @@ class Graph:
     This dataclass encodes all the aligment output (edges, vertics, and nodes)
     wether these are transcript or gold. Based on this graph, we constrcut the oracle
     """
-    pass
 
+    pass
 
 
 class Parser:
@@ -42,17 +69,35 @@ class Parser:
 
     @staticmethod
     def add_special_symbol(text: Text) -> Text:
+        """
+        class means intermediate symbols: 
+            wrap each letter is a symbol class 
+        """
         symbol = text[0]
         for item in text[1:]:
-            symbol += '-' + item
-        return '-' + symbol + '-'
+            symbol += "-" + item
+        return "-" + symbol + "-"
 
 
 class Oracle:
     pass
 
 
+# class State:
+#     transcribed_queue: Sequence[Symbol]
+#     gold_queue: Sequence[Symbol]
+#     # NOTE: this might need to be a Sequence[Graph]
+#     stack: Sequence[Symbol]
+#     # the graph you've constructed so far
+#     graph: Graph
 
 
+# class Graph:
+#     edges: Sequence[Edge]
 
+
+# class Edge:
+#     source: Symbol
+#     destination: Symbol
+#     label: Text
 
--- tests/test_oracle.py
+++ tests/test_oracle.py
@@ -2,59 +2,106 @@
 
 import unittest
 from clu.phontools.alignment.realine import ReAline
-from clu.phontools.alignment.parser import Oracle, Parser, Actions
+from clu.phontools.alignment.parser import (
+    Oracle,
+    Parser,
+    Actions,
+    Symbol,
+    IntermediateSymbol,
+    prepare_symbols,
+    assign_index,
+)
+
+
+"""
+Test `clu.phontools.alignment.parser.prepare_symbols` behaviors
+Test `clu.phontools.alignment.parser.assign_index` behaviors
+"""
+
+
+class SymbolsTests(unittest.TestCase):
+    def prepare_symbols_test(self):
+        """`clu.phontools.alignment.parser.prepare_symbols()` should return the correct enumerations of the actions used by the parser
+        """
+        text = "cat"
+        self.assertEqual(
+            Symbol(text),
+            "cat",
+            f"Symbol() should return 'cat'",
+        )
+        # symbol = Symbol()
+        # text = "cat"
+        # self.assertEqual(
+        #     prepare_symbols(text),
+        #     [NULL, c, NULL, a, NULL, t, NULL],
+        #     f"prepare_symbols() should return [NULL, c, NULL, a, NULL, t, NULL]",
+        # )
+
+    # def assign_index_test(self):
+    #     """`clu.phontools.alignment.parser.assign_index()` should return the correct enumerations of the actions used by the parser
+    #     """
+    #     # list = [NULL, c, NULL, a, NULL, t, NULL]
+    #     # self.assertEqual(
+    #     #     Parser.assign_indexs(list),
+    #     #     "[(0,NULL), (1,c), NULL, a, NULL, t, NULL]",
+    #     #     f"Parser.prepare_symbols() should return [NULL, c, NULL, a, NULL, t, NULL]",
+    #     # )
+    #     pass
+
 
 """
 Test `clu.phontools.alignment.parser.Actions` behaviors
 """
 
-class ActionsTests(unittest.TestCase):
-    def test_actions(self):
-        """`clu.phontools.alignment.parser.Actions.describe()` should return the correct enumerations of the actions used by the parser
-        """
-        assert len(Actions) == 11
 
-        assert Actions.DISCARD_T.describe() == ('DISCARD_T', 1)
-        assert Actions.DISCARD_G.describe() == ('DISCARD_G', 2)
-        assert Actions.SHIFT_T.describe() == ('SHIFT_T', 3)
-        assert Actions.SHIFT_G.describe() == ('SHIFT_G', 4)
-        assert Actions.STACK_SWAP.describe() == ('STACK_SWAP', 5)
-        assert Actions.INSERTION_PRESERVE_COPY_CHILD.describe() == (
-            'INSERTION_PRESERVE_COPY_CHILD', 6)
-        assert Actions.INSERTION_PRESERVE_COPY_PARENT.describe() == (
-            'INSERTION_PRESERVE_COPY_PARENT', 7)
-        assert Actions.DELETION_PRESERVE_COPY_CHILD.describe() == (
-            'DELETION_PRESERVE_COPY_CHILD', 8)
-        assert Actions.DELETION_PRESERVE_COPY_PARENT.describe() == (
-            'DELETION_PRESERVE_COPY_PARENT', 9)
-        assert Actions.ALIGN.describe() == ('ALIGN', 10)
-        assert Actions.SUBSTITUTION.describe() == ('SUBSTITUTION', 11)
+# class ActionsTests(unittest.TestCase):
+#     def test_actions(self):
+#         """`clu.phontools.alignment.parser.Actions.describe()` should return the correct enumerations of the actions used by the parser
+#         """
+#         assert len(Actions) == 11
 
-        
+#         assert Actions.DISCARD_T.describe() == ("DISCARD_T", 1)
+#         assert Actions.DISCARD_G.describe() == ("DISCARD_G", 2)
+#         assert Actions.SHIFT_T.describe() == ("SHIFT_T", 3)
+#         assert Actions.SHIFT_G.describe() == ("SHIFT_G", 4)
+#         assert Actions.STACK_SWAP.describe() == ("STACK_SWAP", 5)
+#         assert Actions.INSERTION_PRESERVE_COPY_CHILD.describe() == (
+#             "INSERTION_PRESERVE_COPY_CHILD",
+#             6,
+#         )
+#         assert Actions.INSERTION_PRESERVE_COPY_PARENT.describe() == (
+#             "INSERTION_PRESERVE_COPY_PARENT",
+#             7,
+#         )
+#         assert Actions.DELETION_PRESERVE_COPY_CHILD.describe() == (
+#             "DELETION_PRESERVE_COPY_CHILD",
+#             8,
+#         )
+#         assert Actions.DELETION_PRESERVE_COPY_PARENT.describe() == (
+#             "DELETION_PRESERVE_COPY_PARENT",
+#             9,
+#         )
+#         assert Actions.ALIGN.describe() == ("ALIGN", 10)
+#         assert Actions.SUBSTITUTION.describe() == ("SUBSTITUTION", 11)
+
 
 """
 Test `clu.phontools.alignment.parser.Parser` behaviors
 """
 
-class ParserTests(unittest.TestCase):
-    def test_add_special_symbol(self):
-        """`clu.phontools.alignment.parser.Parser.add_special_symbol()` should return 'a string with a special character (-) between its letter/phones'
-        :INPUT: cat
-        :OUTPUT: -c-a-t-
-        """
-        text = 'cat'
-        self.assertEqual(
-            Parser.add_special_symbol(text),
-            '-c-a-t-',
-            f"Parser.add_special_symbol() should return '-c-a-t-'"
-        )
 
-
-
-
-
-
-
+# class ParserTests(unittest.TestCase):
+#     def test_add_special_symbol(self):
+#         """`clu.phontools.alignment.parser.Parser.add_special_symbol()` should return 'a string with a special character (-) between its letter/phones'
+#         :INPUT: cat
+#         :OUTPUT: -c-a-t-
+#         """
+#         text = "cat"
+#         self.assertEqual(
+#             Parser.add_special_symbol(text),
+#             "-c-a-t-",
+#             f"Parser.add_special_symbol() should return '-c-a-t-'",
+#         )
 
 
 """
@@ -72,5 +119,3 @@ Test behavior of Oracle
 #             f"Oracle.greet() should return 'hello', but {Oracle.greet()} was returned",
 #         )
 
-
- 
