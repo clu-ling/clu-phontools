3d98ef599d75f2f244c349999ef2b80377fed618
Elsayedissa <elsayed.sabry.issa@gmail.com>
2021-09-12 20:05:45 -0700

    adding stack, state and graph to parser.py

--- clu/phontools/alignment/parser.py
+++ clu/phontools/alignment/parser.py
@@ -31,10 +31,10 @@ class Index:
 
     @staticmethod
     def prepare_symbols(word: Text) -> Sequence[Symbol]:
-        res = [IntermediateSymbol()]
+        res = [str(IntermediateSymbol())]
         for char in word:
-            res.append(Symbol(char))
-            res.append(IntermediateSymbol())
+            res.append(str(Symbol(char)))
+            res.append(str(IntermediateSymbol()))
         return res
 
     @staticmethod
@@ -48,67 +48,121 @@ class Actions(Enum):
     The actions used by the parser.
     """
 
-    DISCARD_T = auto()
-    DISCARD_G = auto()
-    SHIFT_T = auto()
-    SHIFT_G = auto()
-    STACK_SWAP = auto()
-    INSERTION_PRESERVE_COPY_CHILD = auto()
-    INSERTION_PRESERVE_COPY_PARENT = auto()
-    DELETION_PRESERVE_COPY_CHILD = auto()
-    DELETION_PRESERVE_COPY_PARENT = auto()
-    ALIGN = auto()
-    SUBSTITUTION = auto()
+    DISCARD_T = "discard-t"
+    DISCARD_G = "discard-g"
+    SHIFT_T = "shift-t"
+    SHIFT_G = "shift-g"
+    STACK_SWAP = "stack-swap"
+    INSERTION_PRESERVE_COPY_CHILD = "insertion-preserve-copy-child"
+    INSERTION_PRESERVE_COPY_PARENT = "insertion-preserve-copy-parent"
+    DELETION_PRESERVE_COPY_CHILD = "deletion-preserve-copy-child"
+    DELETION_PRESERVE_COPY_PARENT = "deletion-preserve-copy-parent"
+    ALIGN = "align"
+    SUBSTITUTION = "substitution"
+    DELETION = "deletion"
 
     def describe(self):
         return self.name, self.value
 
 
-@dataclass()
-class Graph:
-    """
-    This dataclass encodes all the aligment output (edges, vertics, and nodes)
-    wether these are transcript or gold. Based on this graph, we constrcut the oracle
-    """
+class State:
+    def __init__(self, gold, transcribed):
+        self.gold = gold
+        self.transcribed = transcribed
 
-    pass
+    def gold_queue(self):
+        """
+        returns queue for gold with special symbol (NULL) and indexes
+        [
+        (0, "NULL"),
+        (1, "c"),
+        (2, "NULL"),
+        (3, "a"),
+        (4, "NULL"),
+        (5, "t"),
+        (6, "NULL"),
+    ]
+        """
+        gold_symbols = Index.prepare_symbols(self.gold)
+        return Index.assign_index(gold_symbols)
+
+    def transcribed_queue(self):
+        """
+        returns queue for transcribed with special symbol (NULL) and indexes
+        [
+        (0, "NULL"),
+        (1, "c"),
+        (2, "NULL"),
+        (3, "a"),
+        (4, "NULL"),
+        (5, "t"),
+        (6, "NULL"),
+        (7, "s"),
+        (8, "NULL"),
+    ]
+        """
+        transcribed_symbols = Index.prepare_symbols(self.transcribed)
+        return Index.assign_index(transcribed_symbols)
+
+    def realine_output(self):
+        """
+        returns alignments
+        """
+        aligner = ReAline()
+        return aligner.align(self.gold, self.transcribed)
+
+    def graph(self):
+        """
+        returns a toy graph
+        [('c', 'align', 'c'), ('a', 'align', 'a'), ('t', 'align', 't'), ('s', 'deletion-preserve-copy-parent', '-')]
+
+        This should be integrated with the special symbol (NULL)???!
+        """
+        res = []
+        aligner = ReAline()
+        alignments = aligner.align(self.gold, self.transcribed)
+        for i in alignments:
+            if i[0] == i[1]:
+                res.append((i[0], Actions.ALIGN.value, i[1]))
+            elif i[0] == "-":
+                res.append((i[0], Actions.SUBSTITUTION.value, i[1]))
+            elif i[1] == "-":
+                res.append((i[0], Actions.DELETION.value, i[1]))
+        return res
+
+    def graph_2(self):
+        stack = Stack()
+        for i in self.gold_queue():
+            stack.shift(i)
+        return stack
 
 
-class Parser:
+class Edge:
+    def __init__(self, value):
+        self._value = value
+        self._next = None
+
+
+class Stack:
     def __init__(self):
-        pass
+        self._items = []
 
-    @staticmethod
-    def add_special_symbol(text: Text) -> Text:
-        """
-        class means intermediate symbols:
-            wrap each letter is a symbol class
-        """
-        symbol = text[0]
-        for item in text[1:]:
-            symbol += "-" + item
-        return "-" + symbol + "-"
+    def __str__(self):
+        return str(self._items)
 
+    def shift(self, item):
+        """ shifts an item to the stack: works for shift-t and shift-g"""
+        self._items.append(item)
 
-# class Oracle:
-#     pass
+    def discard(self, item):
+        """ removes an item from the stack: works for discard-t and discard-g"""
+        self._items.remove(item)
 
+    def swap(self, item1, item2):
+        """ swaps two items on the stack"""
+        element1 = self._items.pop(item1)
+        element2 = self._items.pop(item2 - 1)
 
-# class State:
-#     transcribed_queue: Sequence[Symbol]
-#     gold_queue: Sequence[Symbol]
-#     # NOTE: this might need to be a Sequence[Graph]
-#     stack: Sequence[Symbol]
-#     # the graph you've constructed so far
-#     graph: Graph
-
-
-# class Graph:
-#     edges: Sequence[Edge]
-
-
-# class Edge:
-#     source: Symbol
-#     destination: Symbol
-#     label: Text
+        self._items.insert(item1, element2)
+        self._items.insert(item2, element1)
 
